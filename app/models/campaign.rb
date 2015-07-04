932d4e8f452639753aa7f7ec726a58c406559e21
mirkoc <mirko.cerovac@gmail.com>
2015-07-04 22:32:32 +0200

    Added validations to campaign model.
    start_time can not be later than end_time,
    start_time can not be later than Time.now and
    end_time can not be later than Time.now.

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,3 +1,18 @@
 class Campaign < ActiveRecord::Base
   has_many :surfaces
+  validate :possible_start_end_time
+
+
+  private
+  def possible_start_end_time
+    if (start_time.comparable_time > end_time.comparable_time)
+      errors.add(:end_time, 'end_time can not be before start time')
+    end
+    if (end_time.comparable_time < Time.now)
+      errors.add(:end_time, 'end_time can not be before today')
+    end
+    if (start_time.comparable_time < Time.now)
+      errors.add(:start_time, 'start_time can not be before today')
+    end
+  end
 end
