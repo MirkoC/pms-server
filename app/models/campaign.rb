8b57fb6b827ec381a033797d71d2ca613361c007
mirkoc <mirko.cerovac@gmail.com>
2015-07-07 18:36:06 +0200

    Fixed method add_surfaces to add rents.
    Method now creates new Rent (which is connection between surface and campaign). Renamed column 'type' to 'rent_type' in rents table.

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -5,11 +5,23 @@ class Campaign < ActiveRecord::Base
 
   def add_surfaces(surfaces)
     surfaces.each do |surface|
-      surface.update(campaign_id: self[:id])
+      Rent.new(rent_params(surface)).save
+      #surface.update(campaign_id: self[:id])
     end
   end
 
   private
+  def rent_params(surface)
+    status = if (self[:start_time].comparable_time < Time.now and Time.now < self[:end_time].comparable_time)
+               'active'
+             elsif (self[:start_time].comparable_time > Time.now)
+               'not yet started'
+             elsif (self[:end_time].comparabel_time < Time.now)
+               'finished'
+             end
+    {:rent_number => self[:name_of_campaign] + surface[:surface_number].scan(/\d/).join(''), :rent_type => 'campaign', :status => status,
+     :start_time => self[:start_time], :end_time => self[:end_time], :campaign_id => self[:id], :surface_id => surface[:id]}
+  end
   def possible_start_end_time
     if (start_time.comparable_time >= end_time.comparable_time)
       errors.add(:end_time, 'end_time can not be before start time')
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150706193535) do
+ActiveRecord::Schema.define(version: 20150707162900) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 20150706193535) do
 
   create_table "rents", force: :cascade do |t|
     t.string   "rent_number"
-    t.string   "type"
+    t.string   "rent_type"
     t.string   "status"
     t.datetime "start_time"
     t.datetime "end_time"
