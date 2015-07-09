0dbe1bc85e81ae8d889bcc370ec19a35fad89884
mirkoc <mirko.cerovac@gmail.com>
2015-07-09 15:14:37 +0200

    Fix migrations.

--- app/models/surface.rb
+++ app/models/surface.rb
@@ -9,7 +9,7 @@ class Surface < ActiveRecord::Base
   end
 
   def used
-    
+
   end
 
   has_attached_file :image, :styles => { :medium => '300x300>', :thumb => '100x100>' }, :default_url => '/images/:style/missing.png'
