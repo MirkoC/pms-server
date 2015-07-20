e3b8566aa1ea23adf572fdcef526585a2006741f
mirkoc <mirko.cerovac@gmail.com>
2015-07-20 16:19:03 +0200

    Changed views for the surfaces.

--- app/models/surface.rb
+++ app/models/surface.rb
@@ -16,7 +16,7 @@ class Surface < ActiveRecord::Base
 
   end
 
-  has_attached_file :image, :styles => { :medium => '300x300>', :thumb => '100x100>' }, :default_url => '/images/:style/missing.png'
+  has_attached_file :image, :styles => { :medium => '300x300>', :thumb => '100x100>' }, :default_url => '/images/missing.jpg'
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
 end
